dc78c5372bbf94d0369ab3f24fa624b713091e80
Tuyen Ha <98144714+jane-stack@users.noreply.github.com>
2023-09-26 14:40:25 -0600

    navbar buttons

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,9 +1,24 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
 
 function Navbar() {
   return (
     <div className='navbar'>
+      <div className='nav-container'>
+      <div className='nav-child-1'>
+        <div className='menu-container'>
+          <div className='menu-trigger'>
+            <img src='hamicon.png' alt='hamicon' width='20px' />
+          </div>
+        </div>
+      </div>
+      <div className='nav-child-2'>
         <img src='whitelogo.png' alt='rapilogo' width='80px' />
+      </div>
+      <div className='nav-child-3'>
+        <Link>Login</Link>
+      </div>
+      </div>
     </div>
   )
 }

f22e61e7e6877ea62b0b7711c1534c2c79b20384
Tuyen Ha <98144714+jane-stack@users.noreply.github.com>
2023-09-26 17:15:52 -0600

    footers

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -2,8 +2,22 @@ import React from 'react'
 
 function Footer() {
   return (
-    <div>
-        <h3><strong>Our Service and Benefits</strong></h3>
+    <div className='foot-container'>
+      <div className='foot-child-1'>
+        <img src='whitelogo.png' alt='rapilogo' width='60px' />
+      </div>
+      <div className='foot-child-2'>
+        <div className='footer-links'><h5>Object One</h5></div>
+        <div className='footer-links'><h5>Object Two</h5></div>
+        <div className='footer-links'><h5>Object Three</h5></div>
+      </div>
+      <div className='foot-child-3'>
+        <div>
+                <a href="https://www.facebook.com/profile.php?id=61551616851965" target="_blank" rel="noreferrer">
+                    <img src="logo-facebookpng-32236.png" alt="facebooklogo" width='80px' />
+                </a>
+          </div>
+      </div>
     </div>
   )
 }

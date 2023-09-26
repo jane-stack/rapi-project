3c48f87f42502a32447ba506d17050a1e3faf315
Tuyen Ha <98144714+jane-stack@users.noreply.github.com>
2023-09-26 12:30:31 -0600

    created navbar

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,12 @@
-import { useState, useEffect } from "react";
+import React from "react";
+import Navbar from "./Navbar";
 
 function App() {
-  const [count, setCount] = useState(0);
 
-  useEffect(() => {
-    fetch("/hello")
-      .then((r) => r.json())
-      .then((data) => setCount(data.count));
-  }, []);
 
   return (
     <div className="App">
-      <h1>Page Count: {count}</h1>
+      <Navbar />
     </div>
   );
 }

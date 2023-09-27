aca6904160caaeb854cf66d336364d21f14bf24d
Tuyen Ha <98144714+jane-stack@users.noreply.github.com>
2023-09-27 13:04:25 -0600

    car collection startup

--- client/src/components/car/CarPage.js
+++ client/src/components/car/CarPage.js
@@ -2,22 +2,13 @@ import React from 'react'
 
 function CarPage() {
   return (
-    <div>
-      {/* <div className='middlehomediv'>
-        <p><strong>Choose by Brand</strong></p>
-        <br />
-        <br />
-        <br />
-        <br />
-        <p><strong>Choose by Type</strong></p>
-        <br />
-        <br />
-        <br />
-        <br />
-      </div> */}
-      <div className='bottomhomediv'>
-        <h2><strong>Our Impressive Collection of Cars</strong></h2>
-        <p><strong>Here is a demo of the Rapi Rental webpage</strong></p>
+    <div className='bottomhomediv'>
+      <div className='bottom-child-1'>
+      <h3><strong>Our Impressive Collection of Cars</strong></h3>
+      <p>Renting a luxury car has never been easier. Our streamlined process makes it simple for you to book and confirm your vehicle of choice online.</p>
+      </div>
+      <div className='bottom-child-2'>
+      <h3><strong>Book Your Ride</strong></h3>
       </div>
     </div>
   )

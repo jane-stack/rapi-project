4390939ea10cef0f8f0538e3af10307cc3e32864
Tuyen Ha <98144714+jane-stack@users.noreply.github.com>
2023-09-27 13:35:17 -0600

    button for car filter

--- client/src/components/car/CarPage.js
+++ client/src/components/car/CarPage.js
@@ -3,12 +3,14 @@ import React from 'react'
 function CarPage() {
   return (
     <div className='bottomhomediv'>
-      <div className='bottom-child-1'>
       <h3><strong>Our Impressive Collection of Cars</strong></h3>
       <p>Renting a luxury car has never been easier. Our streamlined process makes it simple for you to book and confirm your vehicle of choice online.</p>
-      </div>
-      <div className='bottom-child-2'>
-      <h3><strong>Book Your Ride</strong></h3>
+      <div className='button-div'>
+        <div className='car-btn-div'><button className='car-button'>Popular Car</button></div>
+        <div className='car-btn-div'><button className='car-button'>Luxury Car</button></div>
+        <div className='car-btn-div'><button className='car-button'>Vintage Car</button></div>
+        <div className='car-btn-div'><button className='car-button'>Family Car</button></div>
+        <div className='car-btn-div'><button className='car-button'>Off-Road Car</button></div>
       </div>
     </div>
   )

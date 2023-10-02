9e27e190b1f2e0079a967f52aeede33e7603c5c3
Tuyen Ha <98144714+jane-stack@users.noreply.github.com>
2023-10-02 13:01:20 -0600

    customer review css edits

--- client/src/components/review/ReviewPage.js
+++ client/src/components/review/ReviewPage.js
@@ -4,6 +4,7 @@ function ReviewPage() {
   return (
     <div className='customer-review'>
       <h2>What Our Customers Say</h2>
+      <br />
       <p>“I was really impressed with the level of service I received from this car rental company. The process was smooth and easy, and the car I rented was in excellent condition. The staff was friendly and helpful, and I felt well taken care of throughout my rental period. I would definitely recommend this company to anyone looking for a premium car rental experience.”</p>
       <br />
       <div className='customer-information'>
@@ -12,6 +13,8 @@ function ReviewPage() {
           <strong>Customer Name</strong>
           <br />
           <p className='customer-from'>From Texas</p>
+          <br />
+          <br />
         </div>
       </div>
     </div>

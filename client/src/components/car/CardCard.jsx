3edb1ffbc116a0260314c2d63f6edf94522002be
Tuyen Ha <98144714+jane-stack@users.noreply.github.com>
2023-10-02 11:36:26 -0600

    card fix

--- client/src/components/car/CardCard.jsx
+++ client/src/components/car/CardCard.jsx
@@ -3,16 +3,16 @@ import React from 'react'
 function CardCard() {
   return (
     <div className='image-container'>
-        <div className='car-image-div'><img src='audicar.jpeg' alt='audi2022' className='car-image'/></div>
-        <div className='image-title'>Audi A8 L 2022</div>
-        <div className='image-price'><strong>78.90</strong>/day</div>
+        <div><img src='audicar.jpeg' alt='audi2022' className='car-image'/></div>
         <br />
-        <div className='card-section'>
-
-        </div>
-        <br />
-        <div>
-        <button className='car-detail-btn'>Details</button>
+        <div className='car-body'>
+          <div className='image-title'>Audi A8 L 2022</div>
+          <div className='image-price'><strong>78.90</strong>/day</div>
+          <br />
+            <div className='card-section'>
+            </div>
+          <br />
+          <button className='car-detail-btn'>Details</button>
         </div>
     </div>
   )

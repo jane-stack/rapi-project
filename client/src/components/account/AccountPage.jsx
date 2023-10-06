da7fbc2b383b78a46ae48fa9b72dbb3666fe2bf6
janenguyen <tuyen_ha@outlook.com>
2023-10-05 21:11:10 -0600

    accound nav

--- client/src/components/account/AccountPage.jsx
+++ client/src/components/account/AccountPage.jsx
@@ -11,6 +11,12 @@ function AccountPage () {
                 <header>
                 <img src='accountbanner.png' alt='accountbanner' className='account-banner'/>
                 </header>
+                <div className="account-nav">
+                    <div className="account-btn-div"><button className="account-btn account-border-radius-left">Dashboard</button></div>
+                    <div className="account-btn-div"><button className="account-btn">Profile</button></div>
+                    <div className="account-btn-div"><button className="account-btn">Documents</button></div>
+                    <div className="account-btn-div"><button className="account-btn account-border-radius-right">Upgrade</button></div>
+                </div>
             </div>
         )
     }

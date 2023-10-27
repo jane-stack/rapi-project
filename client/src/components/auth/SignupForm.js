40c646b658f2d04c7c71b93cfb64628012994fed
janenguyen <tuyen_ha@outlook.com>
2023-10-27 11:09:12 -0600

    signup form edit

--- client/src/components/auth/SignupForm.js
+++ client/src/components/auth/SignupForm.js
@@ -60,79 +60,141 @@ function SignupForm() {
     <form className="form-signup" onSubmit={handleSubmit}>
       <img src='whitelogo.png' alt='rapilogo' width='100px' />
       <h3>Create An Account</h3>
-      <div className='input-box'>
-      <input
-      className="login-input"
-      type="text"
-      name="first_name"
-      id="first_name"
-      placeholder="First Name"
-      value={first_name}
-      onChange={(e) => setFirst_name(e.target.value)}
-      />
+      <div className='signup-box-div'>
+        <div>
+          <div className='input-box'>
+          <input
+          className="login-input"
+          type="text"
+          name="first_name"
+          id="first_name"
+          placeholder="First Name"
+          value={first_name}
+          onChange={(e) => setFirst_name(e.target.value)}
+          />
+          </div>
+          <div className='input-box'>
+          <input
+          className="login-input"
+          type="date"
+          name="birthday"
+          id="birthday"
+          placeholder="Birth Date"
+          />
+          </div>
+          <div className='input-box'>
+          <input
+          className="login-input"
+          type="text"
+          name="email"
+          id="email"
+          placeholder="Email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          />
+          </div>
+          <div className='input-box'>
+          <input
+          className="login-input"
+          type="text"
+          name="last_name"
+          id="last_name"
+          placeholder="Phone Number"
+          value={last_name}
+          onChange={(e) => setLast_name(e.target.value)}
+          />
+          </div>
+          <br />
+          <div className='input-box input-with-icon-div'>
+          <input
+          className='custom-input'
+          type={type1}
+          name="password"
+          // id="password"
+          placeholder="Password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          />
+          {type1 === "password" ? (
+          <span className='icon-span' onClick={() => setType1("text")}>
+            <Icon icon={basic_eye_closed} size={15} />
+          </span>
+          ):(
+          <span className='icon-span' onClick={() => setType1("password")}>
+            <Icon icon={basic_eye} size={15} />
+          </span>
+          )}
+          </div>
+          <div className='input-box input-with-icon-div'>
+          <input
+          className="custom-input"
+          type={type2}
+          name="password_confirmation"
+          id="password_confirmation"
+          placeholder="Re-Enter Password"
+          value={password_confirmation}
+          onChange={(e) => setPassword_confirmation(e.target.value)}
+          />
+          {type2 === "password" ? (
+          <span className='icon-span' onClick={() => setType2("text")}>
+            <Icon icon={basic_eye_closed} size={15} />
+          </span>
+          ):(
+          <span className='icon-span' onClick={() => setType2("password")}>
+            <Icon icon={basic_eye} size={15} />
+          </span>
+          )}
+          </div>
       </div>
-      <div className='input-box'>
-      <input
-      className="login-input"
-      type="text"
-      name="last_name"
-      id="last_name"
-      placeholder="Last Name"
-      value={last_name}
-      onChange={(e) => setLast_name(e.target.value)}
-      />
+      
+        <div>
+          <div className='input-box'>
+          <input
+          className="login-input"
+          type="text"
+          name="last_name"
+          id="last_name"
+          placeholder="Last Name"
+          value={last_name}
+          onChange={(e) => setLast_name(e.target.value)}
+          />
+          </div>
+          <div className='input-box'>
+          <input
+          className="login-input"
+          type="text"
+          name="last_name"
+          id="last_name"
+          placeholder="Address Line"
+          value={last_name}
+          onChange={(e) => setLast_name(e.target.value)}
+          />
+          </div>
+          <div className='input-box'>
+          <input
+          className="login-input"
+          type="text"
+          name="last_name"
+          id="last_name"
+          placeholder="City"
+          value={last_name}
+          onChange={(e) => setLast_name(e.target.value)}
+          />
+          </div>
+          <div className='input-box'>
+          <input
+          className="login-input"
+          type="text"
+          name="last_name"
+          id="last_name"
+          placeholder="State"
+          value={last_name}
+          onChange={(e) => setLast_name(e.target.value)}
+          />
+          </div>
+        </div>
       </div>
-      <div className='input-box'>
-      <input
-      className="login-input"
-      type="text"
-      name="email"
-      id="email"
-      placeholder="Email"
-      value={email}
-      onChange={(e) => setEmail(e.target.value)}
-      />
-      </div>
-      <div className='input-box input-with-icon-div'>
-      <input
-      className='custom-input'
-      type={type1}
-      name="password"
-      // id="password"
-      placeholder="Password"
-      value={password}
-      onChange={(e) => setPassword(e.target.value)}
-      />
-      {type1 === "password" ? (
-        <span className='icon-span' onClick={() => setType1("text")}>
-          <Icon icon={basic_eye_closed} size={15} />
-        </span>
-      ):(
-        <span className='icon-span' onClick={() => setType1("password")}>
-          <Icon icon={basic_eye} size={15} />
-        </span>
-      )}
-      </div>
-      <div className='input-box input-with-icon-div'>
-      <input
-      className="custom-input"
-      type={type2}
-      name="password_confirmation"
-      id="password_confirmation"
-      placeholder="Re-Enter Password"
-      value={password_confirmation}
-      onChange={(e) => setPassword_confirmation(e.target.value)}
-      />
-      {type2 === "password" ? (
-        <span className='icon-span' onClick={() => setType2("text")}>
-          <Icon icon={basic_eye_closed} size={15} />
-        </span>
-      ):(
-        <span className='icon-span' onClick={() => setType2("password")}>
-          <Icon icon={basic_eye} size={15} />
-        </span>
-      )}
-      </div>
+      <br />
       <button type="submit" className="login-btn">Create Account</button>
       <Errors />
   </form>

9b3941b6ce4f45fa489197f7c2bd61d1256e8393
Tuyen Ha <98144714+jane-stack@users.noreply.github.com>
2023-11-29 14:13:35 -0700

    seed data after reset

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+User.destroy_all
+
+# Users
+jane = User.create(
+    first_name: "Jane",
+    last_name: "Nguyen",
+    phone_number: "7197253249",
+    email: "tuyen_ha@outlook.com",
+    birthday: "06/04/1988",
+    address_line: "22822 E Union Cir",
+    city: "Aurora",
+    state: "CO",
+    password: "Test1234#"
+)
